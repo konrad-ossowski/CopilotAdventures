223de21a5e35a0f083405ab632ee122918cb66fd
Konrad Ossowski <kossowski@schubergphilis.com>
2024-03-28 17:09:36 +0100

    added figures

--- app.py
+++ app.py
@@ -8,3 +8,38 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+class Creature:
+    def Twirl(self):
+        return 'Twirl'
+
+    def Leap(self):
+        return 'Leap'
+
+    def Spin(self):
+        return 'Spin'
+
+
+class Lox(Creature):
+    pass
+
+
+class Drako(Creature):
+    pass
+
+
+def perform_dance(creature1, move1, creature2, move2):
+    if move1 == move2 == 'Twirl':
+        return 'Fireflies light up the forest.'
+    elif (move1, move2) in [('Leap', 'Spin'), ('Spin', 'Leap')]:
+        return 'Gentle rain starts falling.'
+    elif move1 == 'Spin' and move2 == 'Leap':
+        return 'A rainbow appears in the sky.'
+    else:
+        return 'Other combinations create different effects that you can dream up.'
+
+
+lox = Lox()
+drako = Drako()
+
+print(perform_dance(lox, lox.Twirl(), drako, drako.Spin()))    
